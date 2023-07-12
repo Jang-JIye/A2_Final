a8c2678fbd5169c0da004471c54d1784c0c8c64c
jiyeJang <117057863+Jang-JIye@users.noreply.github.com>
2023-07-12 19:18:56 +0900

    [build] 프로젝트 생성

---
+++ src/main/java/com/example/a2_final/A2FinalApplication.java
@@ -0,0 +1,13 @@
+package com.example.a2_final;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A2FinalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(A2FinalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/a2_final/A2FinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.a2_final;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class A2FinalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
